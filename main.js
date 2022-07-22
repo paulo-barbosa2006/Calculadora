74dfe8fbd1c51164de71d300764df131d471c475
Paulo Barbosa <74267828+paulo-barbosa2006@users.noreply.github.com>
2022-07-22 19:44:40 +0100

    .

--- main.js
+++ main.js
@@ -5,16 +5,15 @@ function calcular(){
     let numtxt = document.querySelector('#number')
     let area = document.querySelector('#area')
 
-    if(number.value.length == 0){
-        alert('[ERRO] Complete o espaço em  branco digitando um número.')
+    if(numtxt.value.length == 0){
+        alert('[ERRO] Preencha o espaço em branco com um número.')
     }else{
         let num = Number(numtxt.value)
         let contador = 1
         area.innerHTML = ''
-        while (contador <= 10){
+        while(contador <= 10){
             let item = document.createElement('option')
-            item.text = `${num} x
-            ${contador} = ${num*contador}`
+            item.text = `${num} x ${contador} = ${num*contador}`
             item.value = `tab${contador}`
             area.appendChild(item)
             contador++
